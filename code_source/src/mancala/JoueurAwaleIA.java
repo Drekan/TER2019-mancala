37e7ad3db0c7d3b50d3209ccf1d0d6f4bbb80d76
SEGURA Bastien <bastienprog@gmail.com>
2019-02-19 09:28:19 +0100

    heuristique H1

--- code_source/src/mancala/JoueurAwaleIA.java
+++ code_source/src/mancala/JoueurAwaleIA.java
@@ -15,4 +15,24 @@ public class JoueurAwaleIA extends JoueurAwale{
 		return caseJouer;
 		
 	}
+	
+	/* Heuristique 1:
+	 * L'objectif de cet heuristique est de minimiser
+	 * le nombre de cases vulnérables
+	 */
+	private double H1(int numeroJoueur,int[] plateau) {
+		int nbCasesVulnerables=0;
+		
+		int debut=(numeroJoueur==0?0:6);
+		int fin=(numeroJoueur==1?6:12);
+		
+		for(int i=debut;i<fin;i++) {
+			if(plateau[i]==1 || plateau[i]==2) {
+				nbCasesVulnerables++;
+			}
+		}
+		
+		return 1-((double)(nbCasesVulnerables)*1/6);
+		
+	}
 }
