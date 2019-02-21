a66fbffd03d8e0d2407a47a3ceb816f9417f306d
Chahinez <chahi26ben@gmail.com>
2019-02-21 16:34:19 +0100

    changement principale

--- code_source/src/mancala/Principale.java
+++ code_source/src/mancala/Principale.java
@@ -19,32 +19,42 @@ public class Principale {
 		System.out.println("nbrgraine init : " + ArbitreAwale.getPartie().getNbrGrainesEnJeu() );
 		
 		while( !ArbitreAwale.finPartie() ) {
-			int coupJoue;
+			int coupJoue = 0;
 			
 			// Entree les coups manuellement a travers la console :
-			/*
-			System.out.println(ArbitreAwale.gestionTour().getNomJoueur() +  " donner coup a jouer : ");
-			int coupJouee = sc.nextInt();
-			 */
 					
 			// gestion tour:
 			Random rand = new Random();
 			if( ArbitreAwale.gestionTour() == ArbitreAwale.getJoueur1()){
-				coupJoue = rand.nextInt(5 - 0 + 1) + 0;
+				do {
+					System.out.println(ArbitreAwale.gestionTour().getNomJoueur() +  " donner coup a jouer : ");
+					coupJoue = sc.nextInt();
+					//coupJoue = rand.nextInt(5 - 0 + 1) + 0;
+				}while( !ArbitreAwale.verifierCoupValide(ArbitreAwale.getJoueur1(),coupJoue) );
 				System.out.println("coup jouee par joueur 1 : " +  coupJoue);
 				System.out.println();
-				if( ArbitreAwale.verifierCoupValide(ArbitreAwale.getJoueur1(),coupJoue, ArbitreAwale.getPartie().getPlateau()) )
-					ArbitreAwale.getJoueur1().jouerUnCoup(coupJoue,ArbitreAwale);
+				ArbitreAwale.getJoueur1().jouerUnCoup(coupJoue,ArbitreAwale);
 			}
 			else {
-				coupJoue = rand.nextInt(11 - 6 + 1) + 6;
+				do {
+					coupJoue = rand.nextInt(11 - 6 + 1) + 6;
+				}while( !ArbitreAwale.verifierCoupValide(ArbitreAwale.getJoueur2(),coupJoue) );
 				System.out.println("coup jouee par joueur 2 : " + coupJoue);
 				System.out.println();
-				if( ArbitreAwale.verifierCoupValide(ArbitreAwale.getJoueur2(),coupJoue, ArbitreAwale.getPartie().getPlateau()) )
-					ArbitreAwale.getJoueur2().jouerUnCoup(coupJoue,ArbitreAwale);
+				ArbitreAwale.getJoueur2().jouerUnCoup(coupJoue,ArbitreAwale);
 			}
 			
+			System.out.println();
 			ArbitreAwale.stockerEtatMouvement(ArbitreAwale.getPartie().etatActuel());
+			for(int i = 11;i>5;i--) {
+				System.out.print(ArbitreAwale.getPartie().etatActuel()[i] + " | ");
+			}
+			System.out.println();
+			for(int i = 0;i<6;i++) {
+				System.out.print(ArbitreAwale.getPartie().etatActuel()[i] + " | ");
+			}
+			
+			System.out.println();
 			
 			System.out.println("Nbr graines en jeu : " + ArbitreAwale.getPartie().getNbrGrainesEnJeu());
 			
