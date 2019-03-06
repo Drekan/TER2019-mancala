7aaba0dfb79d45b99ae6dfd1ddfa2d408893a9c8
EstelleDeBaere <47121088+EstelleDeBaere@users.noreply.github.com>
2019-03-06 10:23:03 +0100

    Correction orthographe

--- code_source/src/mancala/GameManager.java
+++ code_source/src/mancala/GameManager.java
@@ -2,13 +2,13 @@ package mancala;
 
 //C'est l'arbitre d'une partie, il connait le plateau de jeu et les joueurs,
 //c'est lui qui demande aux joueurs de jouer quand leur tour arrive,
-//et c'est lui qui soumet leurs coup au jeu qu'il arbitre (Expl : Awale)
+//et c'est lui qui soumet leurs coups au jeu qu'il arbitre (Expl : Awale)
 public abstract class GameManager {
 
 	//methods :
 	public abstract JoueurAwale gestionTour();//decide de qui va jouer
 	public abstract boolean finPartie();//dire si c'est une fin de partie et arreter le jeu en fonction
-	public abstract void gestionTemps();//gere le temps allouer a chaque joueur tour a tour
+	public abstract void gestionTemps();//gere le temps alloue a chaque joueur tour a tour
 	public abstract void getGagnant() ; // annoncer le gagnant
 	
 }
