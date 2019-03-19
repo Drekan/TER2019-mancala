6b9ae798b145a681f8692c1b8a3c75afef976eb1
SEGURA Bastien <bastienprog@gmail.com>
2019-03-19 09:24:36 +0100

    ajout d'attributs à Tournois

--- code_source/src/mancala/Tournois.java
+++ code_source/src/mancala/Tournois.java
@@ -3,6 +3,22 @@ package mancala;
 public class Tournois {
 
 	static Awale partie;
+	private int nbrVictoiresJ1=0;
+	private int nbrVictoiresJ2=0;
+	
+	public int getNbrVictoireJ1() {
+		return nbrVictoiresJ1;
+	}
+	public void setNbrVictoireJ1(int nbr) {
+		this.nbrVictoiresJ1=nbr;
+	}
+	
+	public int getNbrVictoireJ2() {
+		return nbrVictoiresJ2;
+	}
+	public void setNbrVictoireJ2(int nbr) {
+		this.nbrVictoiresJ2=nbr;
+	}
 	
 	public static void main(String[] args) {
 		GameManagerAwale ArbitreAwale = new GameManagerAwale(0,0);
