2010656538f2ac2bb8d62df8b32d8a88742ffde4
nilbahaaeddine <nilbahaaeddine@gmail.com>
2019-03-21 01:45:57 +0100

    Mise à jour du thème (JPanel et JButton).

--- code_source/src/mancala/DrawingManagerAwale.java
+++ code_source/src/mancala/DrawingManagerAwale.java
@@ -1,11 +1,10 @@
 package mancala;
 
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
+import java.awt.*;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
@@ -177,22 +176,22 @@ public class DrawingManagerAwale {
 		JLabel titre = new JLabel("Bienvenue au jeu d'Awale !");
 		menu.add(titre);
 
-		JPanel game = new JPanel();
+		JPanel game = new SPanel();
 		frmAwale.getContentPane().add(game);
 		game.setLayout(new GridLayout(2, 1, 0, 5));
 
-		JPanel j2 = new JPanel();
+		JPanel j2 = new SPanel();
 		game.add(j2);
 		j2.setLayout(new GridLayout(0, 1, 0, 5));
 
-		JPanel j2_info = new JPanel();
+		JPanel j2_info = new SPanel();
 		j2.add(j2_info);
 
-		JPanel j2_score = new JPanel();
+		JPanel j2_score = new SPanel();
 		j2.add(j2_score);
 		j2_score.setLayout(new GridLayout(1, 6, 10, 0));
 
-		JPanel j2_cases = new JPanel();
+		JPanel j2_cases = new SPanel();
 		j2.add(j2_cases);
 		j2_cases.setLayout(new GridLayout(1, 6, 10, 0));
 
@@ -200,7 +199,7 @@ public class DrawingManagerAwale {
 		buttonList = new ArrayList<>();
 		for(int i = 0; i < 12; i++)
 		{
-			JButton btn = new JButton("4");
+			JButton btn = new SButton("4");
 			final int fi = i;
 			btn.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
@@ -258,11 +257,11 @@ public class DrawingManagerAwale {
 // 			}
 // 		});
 
-		JPanel j1 = new JPanel();
+		JPanel j1 = new SPanel();
 		game.add(j1);
 		j1.setLayout(new GridLayout(0, 1, 0, 5));
 
-		JPanel j1_cases = new JPanel();
+		JPanel j1_cases = new SPanel();
 		j1.add(j1_cases);
 		j1_cases.setLayout(new GridLayout(1, 6, 10, 0));
 
@@ -339,11 +338,11 @@ public class DrawingManagerAwale {
 		// j1_cases.add(j5);
 
 
-		JPanel j1_score = new JPanel();
+		JPanel j1_score = new SPanel();
 		j1.add(j1_score);
 		j1_score.setLayout(new GridLayout(1, 6, 10, 0));
 
-		JPanel j1_info = new JPanel();
+		JPanel j1_info = new SPanel();
 		j1.add(j1_info);
 
 	}
