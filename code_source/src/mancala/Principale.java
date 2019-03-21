793426fa0eb8d3a5c9e37902121a47e3e118dd7b
SEGURA Bastien <bastienprog@gmail.com>
2019-03-21 10:19:15 +0100

    On peut créer un tournois via le menu

--- code_source/src/mancala/Principale.java
+++ code_source/src/mancala/Principale.java
@@ -6,40 +6,48 @@ import java.util.Scanner;
 public class Principale {
 
 	public static void main(String[] args) {
-		
-		/*
-		JoueurAwaleIA j1=new JoueurAwaleIA("IA_TOURNOIS_1",0,1,0,5,24);
-		JoueurAwaleIA j2=new JoueurAwaleIA("IA_TOURNOIS_2",0,2,6,11,24);
-		Tournois tournois=new Tournois(j1,j2);
-		tournois.lancer(); 
-		*/
-		
-		GameManagerAwale arbitreAwale = new GameManagerAwale();
+		int choix;
 		Scanner sc = new Scanner(System.in);
-		int gameMode;
-
-		System.out.println("\n----Choisissez le mode d'affichage----");
-		System.out.println("0. Console");
-		System.out.println("1. Graphique");
-
+		System.out.println("----MENU PRINCIPAL----");
+		System.out.println("0. Jouer");
+		System.out.println("1. Mode Tournois (IA vs IA)");
 		do {
 			System.out.print("\nVotre choix >> ");
-			gameMode = sc.nextInt();
-		}while(gameMode < 0 || gameMode > 1);
-
-		switch (gameMode){
-			//Partie sur console
-			case 0:	arbitreAwale.lancerUneNouvellePartie(true);
-					break;
-			//Partie graphique
-			case 1:	DrawingManagerAwale window = new DrawingManagerAwale(arbitreAwale);
-					window.getFrmAwale().setVisible(true);
-					arbitreAwale.lancerUneNouvellePartieGraphique(window);
-					break;
-		}
-		arbitreAwale.getGagnant();
-		sc.close();
+			choix = sc.nextInt();
+		}while(choix < 0 || choix > 1);
 		
+		if(choix==0) {
+			GameManagerAwale arbitreAwale = new GameManagerAwale();
+	
+			System.out.println("\n----Choisissez le mode d'affichage----");
+			System.out.println("0. Console");
+			System.out.println("1. Graphique");
+	
+			do {
+				System.out.print("\nVotre choix >> ");
+				choix = sc.nextInt();
+			}while(choix < 0 || choix > 1);
+	
+			switch (choix){
+				//Partie sur console
+				case 0:	arbitreAwale.lancerUneNouvellePartie(true);
+						break;
+				//Partie graphique
+				case 1:	DrawingManagerAwale window = new DrawingManagerAwale(arbitreAwale);
+						window.getFrmAwale().setVisible(true);
+						arbitreAwale.lancerUneNouvellePartieGraphique(window);
+						break;
+			}
+			arbitreAwale.getGagnant();
+			
+		}
+		else {
+			JoueurAwaleIA j1=new JoueurAwaleIA("IA_TOURNOIS_1",0,1,0,5,24);
+			JoueurAwaleIA j2=new JoueurAwaleIA("IA_TOURNOIS_2",0,2,6,11,24);
+			Tournois tournois=new Tournois(j1,j2);
+			tournois.lancer(); 
+		}
+		sc.close();
 	}
 
 }
