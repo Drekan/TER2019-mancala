2ebe72343cbf65c1aae70543d4485a76190ff6ed
EstelleDeBaere <47121088+EstelleDeBaere@users.noreply.github.com>
2019-05-03 12:31:26 +0200

    Suppression remise à 0 du temps total d'exécution

--- code_source/src/mancala/GameManagerAwale.java
+++ code_source/src/mancala/GameManagerAwale.java
@@ -426,8 +426,6 @@ public class GameManagerAwale extends GameManager implements Cloneable,java.io.S
 					//DivByZero
 					System.out.println("Temps de calcul moyen d'un coup : " + ((JoueurAwaleIA)this.joueur1).getTotalTime()/((JoueurAwaleIA)this.joueur1).getNombreDeCoup());
 					System.out.println("Nombre de noeuds parcourus en moyenne pour le calcul d'un coup : " + ((JoueurAwaleIA)this.joueur1).getTotalNode()/((JoueurAwaleIA)this.joueur1).getNombreDeCoup());
-					((JoueurAwaleIA)this.joueur1).setTotalTime(0);
-					((JoueurAwaleIA)this.joueur1).setNombreDeCoup(0);
 					System.out.println();
 				}
 
@@ -435,8 +433,6 @@ public class GameManagerAwale extends GameManager implements Cloneable,java.io.S
 				{
 					System.out.println("Temps de calcul moyen d'un coup : " + ((JoueurAwaleIA)this.joueur2).getTotalTime()/((JoueurAwaleIA)this.joueur2).getNombreDeCoup());
 					System.out.println("Nombre de noeuds parcourus en moyenne pour le calcul d'un coup : " + ((JoueurAwaleIA)this.joueur2).getTotalNode()/((JoueurAwaleIA)this.joueur2).getNombreDeCoup());
-					((JoueurAwaleIA)this.joueur2).setTotalTime(0);
-					((JoueurAwaleIA)this.joueur2).setNombreDeCoup(0);
 					System.out.println();
 				}
 				System.out.println(messageFinDePartie);
