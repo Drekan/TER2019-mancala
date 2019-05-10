47f2ada16db301bbebc0ac50b0d58570048a6ff5
nilbahaaeddine <nilbahaaeddine@gmail.com>
2019-05-10 19:51:02 +0200

    Modification affichage IA vs H

--- code_source/src/mancala/GameManager.java
+++ code_source/src/mancala/GameManager.java
@@ -4,14 +4,12 @@ package mancala;
 //c'est lui qui demande aux joueurs de jouer quand leur tour arrive,
 //et c'est lui qui soumet leurs coups au jeu qu'il arbitre (Expl : Awale)
 public abstract class GameManager {
-
 	//methods :
 	public abstract JoueurAwale joueurActuel();//decide de qui va jouer
 
 	public abstract boolean finPartie();//dire si c'est une fin de partie et arreter le jeu en fonction
 
-	public abstract void gestionTemps();//gere le temps alloue a chaque joueur tour a tour
-
 	public abstract JoueurAwale getGagnant(); // annoncer le gagnant
 
+	public abstract void gestionTemps();//gere le temps alloue a chaque joueur tour a tour
 }
